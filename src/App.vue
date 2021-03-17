8122668bac97e53dd20221b4fa5a6d014205c560
Luka Budík <sheffikthegod@gmail.com>
2021-03-17 12:24:02 +0100

    init:web

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <page />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import page from './components/page.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    page
+  }
+})
+</script>
+
+<style>
+
+</style>
