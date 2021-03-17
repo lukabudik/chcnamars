1fe523d07e818871011f2e6f9b96107ff03357aa
Luka Budík <sheffikthegod@gmail.com>
2021-03-17 13:42:06 +0100

    feat: minor changes

--- src/components/page.vue
+++ src/components/page.vue
@@ -14,13 +14,13 @@
           <form class="bg-gray-900 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
             <div class="mb-4">
               <label class="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
-                P
+                Staň se jedním ze šťastlivců a vyhraj letenku na Mars!
               </label>
               <input
                 class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-                id="emailaddress"
+                id="name"
                 type="text"
-                placeholder="you@somewhere.com"
+                placeholder="Jan Novák"
               />
             </div>
 
@@ -29,7 +29,7 @@
                 class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                 type="button"
               >
-                Sign Up
+                Soutěžit
               </button>
             </div>
           </form>
@@ -37,12 +37,12 @@
 
         <!--Right Col-->
         <div class="w-full xl:w-3/5 p-12 overflow-hidden">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/vJEmbs27lCs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="1000" height="500" src="https://www.youtube.com/embed/iG2vu4HE9t0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
         <!--Footer-->
         <div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
-          <a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; App 2020</a>
+          <a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; </a>
           - Template by
           <a class="text-gray-500 no-underline hover:no-underline" href="https://www.tailwindtoolbox.com">TailwindToolbox.com</a>
         </div>
